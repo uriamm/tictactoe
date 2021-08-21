82343d3b13b322da33a1ddbf4098e96ba347f248
uriamm <87574651+uriamm@users.noreply.github.com>
2021-08-21 14:17:13 -0400

    Added the CSS

---
+++ app.js
@@ -0,0 +1,21 @@
+// console.log("working")
+
+
+
+
+const $div = $('<div>')
+
+$('body').append($div)
+console.log($div)
+
+
+const $h1 = $('<h1>')
+
+$h1.text("Tic Cat Toe")
+
+
+
+
+$div.append($h1)
+
+$h1.addClass('ticCatToe')
